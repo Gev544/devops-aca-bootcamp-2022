dba1747286a8a12c9aa4ec880422d6d580a7f137
hoso11 <hovsep.07@mail.ru>
2022-06-16 22:36:23 +0400

    second commit

--- aws_instance1.sh
+++ aws_instance1.sh
@@ -20,7 +20,7 @@
 			 cd Aws_Project
 			 echo "created repository by name Aws_Project"
 			 x=`pwd`
-			 echo "all files can be foundet :" $x
+			 echo "all files can be found in :" $x
 		 }
 
 		aws_cli() {
@@ -37,15 +37,20 @@
 		vpc() {
 
 	### crate vpc and save on file vpc_id
-	aws ec2 create-vpc --cidr-block 192.168.0.0/16 --query Vpc.VpcId --output text > vpc_id
+	vpc_id=$(aws ec2 create-vpc \
+		--cidr-block 192.168.0.0/16 \
+		--query Vpc.VpcId --output text)
 	### crate subnet for vpc
-	vpc_id1=`cat vpc_id`
+	#vpc_id1=`cat vpc_id`
 		}
 
 		subnet() {
 
-	aws ec2 create-subnet --vpc-id $vpc_id1 --cidr-block 192.168.1.0/24 --query Subnet.SubnetId --output text > sub_id
-	sub_id1=`cat sub_id`
+			sub_id=$(aws ec2 create-subnet \
+				--vpc-id $vpc_id \
+				--cidr-block 192.168.1.0/24 \
+				--query Subnet.SubnetId --output text)
+	#sub_id1=`cat sub_id`
 
 	### crate second subnet
 	#aws ec2 create-subnet --vpc-id $vpc_id1 --cidr-block 192.168.2.0/24 --output text > vpc_id2
@@ -54,33 +59,43 @@
 		getway() {
 
 	### crate getway
-	aws ec2 create-internet-gateway --query InternetGateway.InternetGatewayId --output text > igw
-igw1=`cat igw`
+	igw=$(aws ec2 create-internet-gateway \
+		--query InternetGateway.InternetGatewayId --output text)
+	#igw1=`cat igw`
 		}
 
 		attach_getway() {
 
 	### attach-internet-getway to vpc
-	aws ec2 attach-internet-gateway --vpc-id $vpc_id1 --internet-gateway-id $igw1 
+	aws ec2 attach-internet-gateway \
+		--vpc-id $vpc_id \
+		--internet-gateway-id $igw 
 		}
 
 		routing_table() {
 
 	### crate routing table for vpc
-	aws ec2 create-route-table --vpc-id $vpc_id1 --query RouteTable.RouteTableId --output text > rtb
-rtb1=`cat rtb`
+	rtb=$(aws ec2 create-route-table \
+		--vpc-id $vpc_id \
+		--query RouteTable.RouteTableId --output text) 
+	#rtb1=`cat rtb`
 		}
 		route() {
 
 	### crate route for all trafic 0.0.0.0/0
-	aws ec2 create-route --route-table-id $rtb1 --destination-cidr-block 0.0.0.0/0 --gateway-id $igw1
+	aws ec2 create-route \
+		--route-table-id $rtb \
+		--destination-cidr-block 0.0.0.0/0 \
+		--gateway-id $igw
 	##aws ec2 describe-route-tables --route-table-id rtb1
 		}
 
 		associetione() {
 
 	### associate subnet to route table
-	aws ec2 associate-route-table  --subnet-id $sub_id1 --route-table-id $rtb1 > associate1
+	associate1=$(aws ec2 associate-route-table  \
+		--subnet-id $sub_id \
+		--route-table-id $rtb)
          
 	### show subnet associetione
 	## aws ec2 describe-subnets --filters "Name=vpc-id,Values=vpc-2f09a348" --query "Subnets[*].{ID:SubnetId,CIDR:CidrBlock}"
@@ -89,7 +104,9 @@ rtb1=`cat rtb`
 		kay_pair() {
 
 	### crate key pair
-	aws ec2 create-key-pair --key-name MyKeyPair1 --query 'KeyMaterial' --output text > MyKeyPair1.pem
+	aws ec2 create-key-pair \
+		--key-name MyKeyPair1 \
+		--query 'KeyMaterial' --output text > MyKeyPair1.pem
 
 	### change permitione key pair
 	chmod 400 MyKeyPair1.pem
@@ -104,16 +121,24 @@ rtb1=`cat rtb`
 		security_group() {
 
 	### crate security group
-	aws ec2 create-security-group --group-name SSHAccess --description "Security group for SSH access" --vpc-id $vpc_id1 > security_group
-	sec_group=`cat security_group`
+	security_group=$(aws ec2 create-security-group \
+		--group-name SSHAccess \
+		--description "Security group for SSH access" \
+		--vpc-id $vpc_id) 
+	#sec_group=`cat security_group`
 		}
 
 	 
 		autorize_sec_grp() {
 
 	### crate security group  
-	aws ec2 authorize-security-group-ingress --group-id $sec_group --protocol tcp --port 22 --cidr 0.0.0.0/0 > allowed
-	ssh1=`cat $x/"allowed" | head -n 1`
+	allowed=$(aws ec2 authorize-security-group-ingress \
+		--group-id $security_group \
+		--protocol tcp \
+		--port 22 \
+		--cidr 0.0.0.0/0)
+		ssh1=$(echo $allowed | cut -c 1-4)
+		#echo $allowed
 		}
 
 		run_instance() {
@@ -122,7 +147,14 @@ rtb1=`cat rtb`
 	###               ami-0c4f7023847b90238 (64-bit (x86)) ubuntu 20.04 free
 	###               ami-09d56f8956ab235b3 (64-bit (x86)) ubuntu 22.04 free
 	###               ami-0193dcf9aa4f5654e (64-bit (x86)  windows server 2019 free
-	aws ec2 run-instances     --image-id ami-0c4f7023847b90238     --instance-type t2.micro     --subnet-id $sub_id1     --security-group-ids $sec_group     --associate-public-ip-address     --key-name MyKeyPair1 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=test01}]' > Instans_configurationes
+	aws ec2 run-instances     \
+		--image-id ami-0c4f7023847b90238     \
+		--instance-type t2.micro     \
+		--subnet-id $sub_id     \
+		--security-group-ids $security_group     \
+		--associate-public-ip-address     \
+		--key-name MyKeyPair1 \
+		--tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=test01}]' > Instans_configurationes
 
 		}
 
@@ -142,27 +174,39 @@ rtb1=`cat rtb`
 	fi
 	# crate vpc
 	vpc
-	if [[ ! -s $x/"vpc_id" ]]
+	if [[ -z $vpc_id ]]
 	then
 		echo "vpc was not created"
+		aws ec2 delete-vpc \
+		--vpc-id $vpc_id
 		exit
 	else
 		echo "vpc created"
 	fi
 	# crate subnet
 	subnet
-	if [[ ! -s $x/"sub_id" ]]
+	if [[ -z $sub_id ]]
         then
                 echo "subnet was not created"
+		aws ec2 delete-subnet \
+		--subnet-id $sub_id
+		aws ec2 delete-vpc \
+                --vpc-id $vpc_id
                 exit
         else
                 echo "subnet created"
         fi
 	# crate internet_getway
 	getway
-	if [[ ! -s $x/"igw" ]]
+	if [[ -z $igw ]]
         then
                 echo "internet_getway was not created"
+		aws ec2 delete-internet-gateway \
+    		--internet-gateway-id $igw
+		aws ec2 delete-subnet \
+                --subnet-id $sub_id
+                aws ec2 delete-vpc \
+                --vpc-id $vpc_id
                 exit
         else
                 echo "internet_getway created"
@@ -171,27 +215,57 @@ rtb1=`cat rtb`
 	attach_getway
 	# crate route table for all trafic 0.0.0.0/0
 	routing_table
-	if [[ ! -s $x/"rtb" ]]
+	if [[ -z $rtb ]]
         then
                 echo "route_table was not created"
+		aws ec2 delete-route-table \
+		--route-table-id $rtb
+		aws ec2 delete-internet-gateway \
+                --internet-gateway-id $igw
+		aws ec2 delete-subnet \
+                --subnet-id $sub_id
+		aws ec2 delete-vpc \
+                --vpc-id $vpc_id
                 exit
         else
                 echo "route_table created"
         fi
 	# associate subnet to route table
 	associetione
-	if [[ ! -s $x/"associate1" ]]
+	if [[ -z $associate1 ]]
         then
                 echo "was not associated"
+		aws ec2 disassociate-route-table \
+		--association-id $associate1
+		aws ec2 delete-route-table \
+                --route-table-id $rtb
+                aws ec2 delete-internet-gateway \
+                --internet-gateway-id $igw
+                aws ec2 delete-subnet \
+                --subnet-id $sub_id
+                aws ec2 delete-vpc \
+                --vpc-id $vpc_id
                 exit
         else
                 echo "associate"
         fi
 	# create security_group
 	security_group
-	if [[ ! -s $x/"security_group" ]]
+	if [[ -z $security_group ]]
         then
                 echo "security_group was not crieted"
+		aws ec2 delete-security-group \
+		--group-id $security_group
+		aws ec2 disassociate-route-table \
+                --association-id $associate1
+                aws ec2 delete-route-table \
+                --route-table-id $rtb
+                aws ec2 delete-internet-gateway \
+                --internet-gateway-id $igw
+                aws ec2 delete-subnet \
+                --subnet-id $sub_id
+                aws ec2 delete-vpc \
+                --vpc-id $vpc_id
                 exit
         else
                 echo "security_group crieted"
@@ -201,6 +275,18 @@ rtb1=`cat rtb`
 	if [[ $ssh1 != True ]]
         then
                 echo "ssh conectione not allowed"
+		aws ec2 delete-security-group \
+                --group-id $security_group
+                aws ec2 disassociate-route-table \
+                --association-id $associate1
+                aws ec2 delete-route-table \
+                --route-table-id $rtb
+                aws ec2 delete-internet-gateway \
+                --internet-gateway-id $igw
+                aws ec2 delete-subnet \
+                --subnet-id $sub_id
+                aws ec2 delete-vpc \
+                --vpc-id $vpc_id
                 exit
         else
                 echo "ssh conectione allowed"
@@ -211,6 +297,21 @@ rtb1=`cat rtb`
 	if [[ ! -s $x/"MyKeyPair1.pem" ]]
         then
                 echo "key_pair was not genereted"
+		aws ec2 delete-key-pair \
+                --key-name MyKeyPair1
+                echo "ssh conectione not allowed"
+                aws ec2 delete-security-group \
+                --group-id $security_group
+                aws ec2 disassociate-route-table \
+                --association-id $associate1
+                aws ec2 delete-route-table \
+                --route-table-id $rtb
+                aws ec2 delete-internet-gateway \
+                --internet-gateway-id $igw
+                aws ec2 delete-subnet \
+                --subnet-id $sub_id
+                aws ec2 delete-vpc \
+                --vpc-id $vpc_id
                 exit
         else
                 echo "key_pair genereted"
@@ -220,6 +321,21 @@ rtb1=`cat rtb`
 	if [[ ! -s $x/"Instans_configurationes" ]]
         then
                 echo "instance was not created"
+		aws ec2 delete-key-pair \
+		--key-name MyKeyPair1
+		echo "ssh conectione not allowed"
+                aws ec2 delete-security-group \
+                --group-id $security_group
+                aws ec2 disassociate-route-table \
+                --association-id $associate1
+                aws ec2 delete-route-table \
+                --route-table-id $rtb
+                aws ec2 delete-internet-gateway \
+                --internet-gateway-id $igw
+                aws ec2 delete-subnet \
+                --subnet-id $sub_id
+                aws ec2 delete-vpc \
+                --vpc-id $vpc_id
                 exit
         else
                 cat "$x/Instans_configurationes"
