8350933da3e9072cfe24041e1cb19824b453a566
Hayko S <blabla@gmail.com>
2022-06-17 19:21:41 +0400

    added some options

--- Homework-june-13/create-aws-script.sh
+++ Homework-june-13/create-aws-script.sh
@@ -3,7 +3,6 @@
 set -e
 
 #Delete keys if we want to run script 2nd time
-
 aws ec2 delete-key-pair --key-name aws-homework-key
 
 if [[ -f aws-homework-key.pem ]]
@@ -13,58 +12,67 @@ fi
 
 #Create a VPC with a 10.0.0.0/16 CIDR block
 
-aws ec2 create-vpc --cidr-block 10.0.0.0/16 --query Vpc.VpcId --output text > vpcID.txt
-vpcID=`cat vpcID.txt`
+aws ec2 create-vpc --cidr-block 10.0.0.0/16 --query Vpc.VpcId --output text > IDs.txt
+vpcID=`cat IDs.txt`
 echo "new VPC ID is : $vpcID"
+
+##Giving a DNS attribute
+#aws ec2 modify-vpc-attribute --vpc-id $vpcID --enable-dns-support "{\"Value\":true}"
 
 #Create a first subnet with a 10.0.1.0/24 CIDR block
 
-aws ec2 create-subnet --vpc-id $vpcID --cidr-block 10.0.1.0/24 --query Subnet.SubnetId --output text > subnet1_ID.txt
-subnet1_ID=`cat subnet1_ID.txt`
+aws ec2 create-subnet --vpc-id $vpcID --cidr-block 10.0.1.0/24 --query Subnet.SubnetId --output text >> IDs.txt
+subnet1_ID=`cat IDs.txt | tail -1`
 echo "new Subnet ID is : $subnet1_ID"
 
 #Create a second subnet with a 10.0.2.0/24 CIDR block
 
-aws ec2 create-subnet --vpc-id $vpcID --cidr-block 10.0.2.0/24 --query Subnet.SubnetId --output text > subnet2_ID.txt
-subnet2_ID=`cat subnet2_ID.txt`
+aws ec2 create-subnet --vpc-id $vpcID --cidr-block 10.0.2.0/24 --query Subnet.SubnetId --output text >> IDs.txt
+subnet2_ID=`cat IDs.txt | tail -1`
 echo "second Subnet ID is : $subnet2_ID"
 
 #Create an internet gateway
 
-aws ec2 create-internet-gateway --query InternetGateway.InternetGatewayId --output text > igwID.txt
-igwID=`cat igwID.txt`
+aws ec2 create-internet-gateway --query InternetGateway.InternetGatewayId --output text >> IDs.txt
+igwID=`cat IDs.txt | tail -1`
 echo "Internet Gateway ID is : $igwID"
 
 #Attach the internet gateway to  VPC 
 
 aws ec2 attach-internet-gateway --vpc-id $vpcID --internet-gateway-id $igwID
+echo "Attach the internet gateway to VPC"
 
 #Create a custom route table for  VPC 
 
-aws ec2 create-route-table --vpc-id $vpcID --query RouteTable.RouteTableId --output text > rtID.txt
-rtID=`cat rtID.txt`
+aws ec2 create-route-table --vpc-id $vpcID --query RouteTable.RouteTableId --output text >> IDs.txt
+rtID=`cat IDs.txt | tail -1`
 echo "Route Table ID is : $rtID"
 
 #Create a route in the route table that points all traffic (0.0.0.0/0) to the internet gateway
 
 aws ec2 create-route --route-table-id $rtID --destination-cidr-block 0.0.0.0/0 --gateway-id $igwID
+echo "Create a route in the route table with 0.0.0.0/0"
 
-#Associate a subnet with the custom route table, we make our subnet1 public
+#Associate a subnet with the custom route table, we make our subnet public
 
-aws ec2 associate-route-table  --subnet-id $subnet1_ID --route-table-id $rtID
+aws ec2 associate-route-table  --subnet-id $subnet1_ID --route-table-id $rtID >> IDs.txt
+
+echo "Public Subnet is associated with custom route table"
 
 #Create a key pair and pipe your private key directly into a file with the .pem extension
 
 aws ec2 create-key-pair --key-name aws-homework-key --query "KeyMaterial" --output text > aws-homework-key.pem
+echo "Key Pair created"
 
 #Change file permisions
 
 chmod 400 aws-homework-key.pem
+echo "key permissions are changed to 400"
 
 #Create a security group in  VPC
 
-aws ec2 create-security-group --group-name my-homework-SG --description "SG for homework SSH access" --vpc-id $vpcID > sgID.txt
-sgID=`cat sgID.txt |head -2|tail -1| cut -d '"' -f 4`
+aws ec2 create-security-group --group-name my-homework-SG --description "SG for homework SSH access" --vpc-id $vpcID >> IDs.txt
+sgID=`cat IDs.txt |tail -2|head -1| cut -d '"' -f 4`
 echo "Security Group ID is : $sgID"
 
 #Add a rule that allows SSH and HTTP access from anywhere
@@ -74,5 +82,16 @@ aws ec2 authorize-security-group-ingress --group-id $sgID --protocol tcp --port 
 
 #Launch 1 t2.micro instance into your public subnet, using the security group and key pair
 
-aws ec2 run-instances --image-id ami-09d56f8956ab235b3 --count 1 --instance-type t2.micro --key-name aws-homework-key --associate-public-ip-address --security-group-ids $sgID --subnet-id $subnet1_ID 
+aws ec2 run-instances --image-id ami-09d56f8956ab235b3 --count 1 --instance-type t2.micro --key-name aws-homework-key --associate-public-ip-address --security-group-ids $sgID --subnet-id $subnet1_ID --query 'Instances[*].InstanceId'  --output text >> IDs.txt
+instanceID=`cat IDs.txt | tail -1`
+echo "Instance ID is : $instanceID"
+
+sleep 30 
+
+#Checking Instance Status and Public IP address
+
+aws ec2 describe-instances --instance-id $instanceID --query "Reservations[*].Instances[*].{State:State.Name,Address:PublicIpAddress}"
+
+
+
 
---
+++ Homework-june-13/delete-aws-script.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+set -e
+
+instanceID=`cat IDs.txt | tail -1`
+sgID=`cat IDs.txt |tail -3|head -1| cut -d '"' -f 4`
+rtID=`cat IDs.txt |head -5| tail -1`
+subnet1_ID=`cat IDs.txt | head -2 | tail -1`
+subnet2_ID=`cat IDs.txt | head -3 | tail -1`
+igwID=`cat IDs.txt | head -4 | tail -1`
+vpcID=`cat IDs.txt | head -1`
+rtassocID=`cat IDs.txt |tail -9|head -1| cut -d '"' -f 4`
+
+
+#Terminating instance
+aws ec2 terminate-instances --instance-ids $instanceID
+echo "Terminating INSTANCE !"
+sleep 60
+
+#Deleting Security Group
+
+aws ec2 delete-security-group --group-id $sgID
+echo "Deleting Security Group !"
+
+#Disassociate a route table
+
+aws ec2 disassociate-route-table --association-id $rtassocID
+echo "Disassociating a Route Table !"
+
+#Deleting Route Table
+
+aws ec2 delete-route-table --route-table-id $rtID
+echo "Deleting Route Table !"
+
+#Deleting Subnets
+
+aws ec2 delete-subnet --subnet-id $subnet1_ID
+aws ec2 delete-subnet --subnet-id $subnet2_ID
+echo "Deleting Subnets !"
+
+#Detaching Internet Gateway
+
+aws ec2 detach-internet-gateway --internet-gateway-id $igwID --vpc-id $vpcID
+echo "Detaching Internet Gateway !"
+
+#Deleting Internet Gateway
+
+aws ec2 delete-internet-gateway --internet-gateway-id $igwID
+echo "Deleting Internet Gateway !"
+
+#Deleting VPC
+
+aws ec2 delete-vpc --vpc-id $vpcID
+echo "Deleting VPC !!!"
+
+#Deleting Keys
+aws ec2 delete-key-pair --key-name aws-homework-key
+
+if [[ -f aws-homework-key.pem ]]
+then
+rm -f aws-homework-key.pem IDs.txt
+fi
+
+echo "--------EVERYTHING IS DELETED---------"
