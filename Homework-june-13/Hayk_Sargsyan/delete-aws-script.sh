eda415a518df2f6f3f011e38045b6c920b0df4fe
Hayko S <blabla@gmail.com>
2022-06-18 05:19:06 +0400

    make code prettyer and create a new BONUS file with functions (fail tolerant)

--- Homework-june-13/Hayk_Sargsyan/delete-aws-script.sh
+++ Homework-june-13/Hayk_Sargsyan/delete-aws-script.sh
@@ -13,48 +13,60 @@ vpcID=`cat IDs.txt | grep vpc-`
 
 
 #Terminating instance
-aws ec2 terminate-instances --instance-ids $instanceID
+aws ec2 terminate-instances \
+	--instance-ids $instanceID
 echo "Terminating INSTANCE !"
-aws ec2 wait instance-terminated --instance-ids $instanceID
+aws ec2 wait instance-terminated \
+	--instance-ids $instanceID
 
 #Deleting Security Group
 
-aws ec2 delete-security-group --group-id $sgID
+aws ec2 delete-security-group \
+	--group-id $sgID
 echo "Deleting Security Group !"
 
 #Disassociate a route table
 
-aws ec2 disassociate-route-table --association-id $rtassocID
+aws ec2 disassociate-route-table \
+	--association-id $rtassocID
 echo "Disassociating a Route Table !"
 
 #Deleting Route Table
 
-aws ec2 delete-route-table --route-table-id $rtID
+aws ec2 delete-route-table \
+	--route-table-id $rtID
 echo "Deleting Route Table !"
 
 #Deleting Subnets
 
-aws ec2 delete-subnet --subnet-id $subnet1_ID
-aws ec2 delete-subnet --subnet-id $subnet2_ID
+aws ec2 delete-subnet \
+	--subnet-id $subnet1_ID
+aws ec2 delete-subnet \
+	--subnet-id $subnet2_ID
 echo "Deleting Subnets !"
 
 #Detaching Internet Gateway
 
-aws ec2 detach-internet-gateway --internet-gateway-id $igwID --vpc-id $vpcID
+aws ec2 detach-internet-gateway \
+	--internet-gateway-id $igwID \
+	--vpc-id $vpcID
 echo "Detaching Internet Gateway !"
 
 #Deleting Internet Gateway
 
-aws ec2 delete-internet-gateway --internet-gateway-id $igwID
+aws ec2 delete-internet-gateway \
+	--internet-gateway-id $igwID
 echo "Deleting Internet Gateway !"
 
 #Deleting VPC
 
-aws ec2 delete-vpc --vpc-id $vpcID
+aws ec2 delete-vpc \
+	--vpc-id $vpcID
 echo "Deleting VPC !!!"
 
 #Deleting Keys
-aws ec2 delete-key-pair --key-name aws-homework-key
+aws ec2 delete-key-pair \
+	--key-name aws-homework-key
 
 if [[ -f aws-homework-key.pem ]]
 then
