3c66779a695d31c81a181893da44f2e38abc091b
NarekGedeonyan <gedeonyan@gmail.com>
2022-06-30 14:54:13 +0400

    cant redirect some remote errors

---
+++ homework-june-15/NarekGedeonyan/install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# This script will create all required aws resources and launch ec2 instance from IAM........... acaunt in us-west 2 region, Please make sure that you know what are you doing. It is required to have 'ec2, s3, nginx, sources.list' files in the same directory as install.sh and uninstall.sh
+#Resource names, ID-s, IP-s and other information abault created resources will be stored in sources.list untill uninstallation
+# !!!!!!!!!!!!!!Do not run this script more than once!!!!!!!!!!!!!!!! otherwise you would have to manualy delete all created resources
+#If something goes wrong please manually run uninstall.sh to delete all created resources
+
+source sources.list 2>/dev/null
+set -e
+
+#Run script to create s3 bucket
+bash ./s3
+echo 's3 script ended'
+#Run script to launch ec2
+bash ./ec2
+echo "$PUBLIC_IP instance launched"
+
+#Run remote commands
+cat ./nginx | ssh -o StrictHostKeyChecking=no -i /home/narek/.ssh/sshkeygenerated.pem  ubuntu@$PUBLIC_IP 'sudo bash -s' 2>./logs
+#cat ./testfile	| ssh -o StrictHostKeyChecking=no -i /home/narek/.ssh/sshkeygenerated.pem  ubuntu@$PUBLIC_IP 'sudo bash -s' 2>./logs
+
+
+
+#ssh to EC2
+#ssh -o StrictHostKeyChecking=no -i /home/$(whoami)/.ssh/sshkeygenerated.pem ubuntu@$PUBLIC_IP
+#sleep 5
+
+#ssh ubuntu@$PUBLIC_IP echo "thats All" 
+
+#if [ ! ssh $PUBLIC_IP ];
+#then    
+#echo "no ssh"
+#else
+#echo "ssh connection established"
+#fi
+
